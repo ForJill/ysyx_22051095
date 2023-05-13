0795215a73e68d0f43462737db432fafcdac749a
ljw <liaojiawei20@mails.ucas.ac.cn>
2023-05-12 21:57:29 -0700

    自动执行image文件

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -26,6 +26,6 @@ image: $(IMAGE).elf
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
-
+	@cp $(IMAGE).bin $(NPC_HOME)/bin/image.bin
 run: image
-	$(MAKE) -C $(NPC_HOME) ISA=$(ISA) run IMG=$(IMAGE).bin
+	$(MAKE) -C $(NPC_HOME) run
